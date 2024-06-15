d66d1ba16b1791b2a88509e638e34daa497f5031
ovictorvasconcelos <victordevelopervasconcelos2006@gmail.com>
2024-06-15 12:56:23 +0100

    :sparkles: feat: add argument parsing and usage instructions

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,24 @@
 #!/usr/bin/env node
+import arg from "arg";
+
+try {
+    const args = arg({
+        '--create': Boolean,
+        '--config': Boolean,
+    });
+
+    if (args['--config'])
+        console.log('Configure the app');
+
+} catch (error) {
+    console.log(error.message);
+    console.log(' ');
+
+    usageTool();
+}
+
+const usageTool = () => {
+    console.log(`Wizend [CMD]
+    --create\tStarts the app
+    --config\tSee app config`);
+}
