b79288bb337806de9da3d180246fc0098fe9b958
ovictorvasconcelos <victordevelopervasconcelos2006@gmail.com>
2024-06-15 13:06:06 +0100

    :sparkles: feat: add custom logger with chalk and debug support

---
+++ src/logger.js
@@ -0,0 +1,12 @@
+import chalk from "chalk";
+import debug from "debug";
+
+export default createLogger = (name) => {
+    return {
+        debug: debug(name),
+        log: (...args) => console.log(chalk.gray(...args)),
+        error: (...args) => console.log(chalk.red(...args)),
+        warning: (...args) => console.log(chalk.yellow(...args)),
+        highlight: (...args) => console.log(chalk.green(...args)),
+    }
+}
