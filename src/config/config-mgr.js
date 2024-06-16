44fef4055624cedbc91d336affe666fffe5ae9e0
ovictorvasconcelos <victordevelopervasconcelos2006@gmail.com>
2024-06-16 11:17:51 +0100

    :sparkles: feat: use the 'ajv' library to handle configuration errors

--- src/config/config-mgr.js
+++ src/config/config-mgr.js
@@ -1,6 +1,10 @@
+import Ajv from "ajv";
 import logger from "../logger.js";
 import { cosmiconfigSync } from "cosmiconfig";
+import betterAjvErrors from "better-ajv-errors";
+import schema from "./schema.json" assert { type: 'json' };
 
+const ajv = new Ajv();
 const logMessage = logger('config:mgr');
 const configLoader = cosmiconfigSync('tool');
 
@@ -11,6 +15,16 @@ export default function getUserConfig() {
         logMessage.error('Could not find configuration, using default');
         return { port: 8080 };
     } else {
+        const validate = ajv.compile(schema);
+        const isValidConfig = validate(resultConfig.config);
+
+        if (!isValidConfig) {
+            logMessage.error('Invalid configuration was supplied');
+            console.log();
+            logMessage.error(betterAjvErrors(schema, resultConfig.config, validate.errors));
+            process.exit(1);
+        }
+
         logMessage.debug('Found configuration (package.json file)', resultConfig.config);
         return resultConfig.config;
     }
