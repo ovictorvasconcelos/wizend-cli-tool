c444e7db3338d9d141c45d7e1d06937ddfc1630c
ovictorvasconcelos <victordevelopervasconcelos2006@gmail.com>
2024-06-16 20:17:13 +0100

    :sparkles: chore: remove the 'start' command from index.js

--- bin/index.js
+++ bin/index.js
@@ -1,24 +1,16 @@
 #!/usr/bin/env node
 import arg from "arg";
 import logger from "../src/logger.js";
-import startCommand from "../src/commands/start.js";
-import getUserConfig from "../src/config/config-mgr.js";
 import { createCommand } from "../src/commands/create.js";
 
 const logMessage = logger('config:mgr');
 
 try {
     const args = arg({
-        '--start': Boolean,
         '--create': Boolean,
     });
 
     logMessage.debug('Received args', args);
-
-    if (args['--start']) {
-        const userConfig = getUserConfig();
-        startCommand(userConfig);
-    }
 
     if (args['--create'])
         createCommand();
@@ -32,6 +24,5 @@ try {
 
 function usageTool() {
     logMessage.log(`Wizend [CMD]
-    --create\tCreate a project
-    --start\tStarts the project`);
+    --create\tCreate a project`);
 }
