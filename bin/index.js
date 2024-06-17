6f72dc22e905f23049214f15d8e1bb618e933ad4
ovictorvasconcelos <victordevelopervasconcelos2006@gmail.com>
2024-06-17 11:30:59 +0100

    :sparkles: feat: add the link command to index.js

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,7 @@ import arg from "arg";
 import logger from "../src/logger.js";
 import { createCommand } from "../src/commands/create.js";
 import { deleteCommand } from "../src/commands/delete.js";
+import { linkCommand } from "../src/commands/linkRepository.js";
 import { listRepositoriesCommand } from "../src/commands/listRepositories.js";
 
 const logMessage = logger('config:mgr');
@@ -11,6 +12,7 @@ try {
     const args = arg({
         '--create': Boolean,
         '--delete': Boolean,
+        '--link': Boolean,
         '--list-repos': Boolean
     });
 
@@ -21,6 +23,9 @@ try {
 
     if (args['--delete'])
         deleteCommand();
+
+    if (args['--link'])
+        linkCommand();
 
     if (args['--list-repos'])
         listRepositoriesCommand();
@@ -36,5 +41,6 @@ function usageTool() {
     logMessage.log(`Wizend [CMD]
     --create\t\tCreate a project
     --delete\t\tDelete a project
+    --link\t\tLink to a Git repository
     --list-repos\tList local Git repositories`);
 }
