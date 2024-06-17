271df9cc583691f314477491228c45c4697b579e
ovictorvasconcelos <victordevelopervasconcelos2006@gmail.com>
2024-06-17 10:21:06 +0100

    :sparkles: feat: add the delete command to index.js

--- bin/index.js
+++ bin/index.js
@@ -2,18 +2,23 @@
 import arg from "arg";
 import logger from "../src/logger.js";
 import { createCommand } from "../src/commands/create.js";
+import { deleteCommand } from "../src/commands/delete.js";
 
 const logMessage = logger('config:mgr');
 
 try {
     const args = arg({
         '--create': Boolean,
+        '--delete': Boolean,
     });
 
     logMessage.debug('Received args', args);
 
     if (args['--create'])
         createCommand();
+
+    if (args['--delete'])
+        deleteCommand();
 
 } catch (error) {
     logMessage.error(error.message);
@@ -24,5 +29,6 @@ try {
 
 function usageTool() {
     logMessage.log(`Wizend [CMD]
-    --create\tCreate a project`);
+    --create\tCreate a project
+    --delete\tDelete a project`);
 }
