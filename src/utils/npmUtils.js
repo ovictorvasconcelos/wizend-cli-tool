8da353b872259ef85292b630e476726bc04f562c
ovictorvasconcelos <victordevelopervasconcelos2006@gmail.com>
2024-06-17 11:10:16 +0100

    :sparkles: feat: create a separate file to execute the 'npm install' command

---
+++ src/utils/npmUtils.js
@@ -0,0 +1,17 @@
+import logger from "../logger.js";
+import { exec } from "child_process";
+
+const execAsync = promisify(exec);
+const logMessage = logger('config:mgr');
+
+export async function runNpmInstall(directory) {
+    try {
+        logMessage.log(' ');
+        logMessage.log('Running npm install...');
+        await execAsync('npm install', { cwd: directory });
+        logMessage.log(' ');
+        logMessage.highlight('Dependencies successfully installed!');
+    } catch (error) {
+        logMessage.error(`Error executing the 'npm install' command: `, error);
+    }
+}
