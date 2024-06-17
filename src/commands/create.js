ef7dccdcf13130bc56af4e3de00b9c1feb601e1b
ovictorvasconcelos <victordevelopervasconcelos2006@gmail.com>
2024-06-17 11:15:11 +0100

    :sparkles: feat: remove the function of connecting to a git repository for another file

--- src/commands/create.js
+++ src/commands/create.js
@@ -3,28 +3,15 @@ import path from "path";
 import inquirer from "inquirer";
 import { promisify } from "util";
 import logger from "../logger.js";
-import { exec } from "child_process";
 import { runNpmInstall } from "../utils/npmUtils.js";
+import { linkGitRepository } from "../utils/gitUtils.js";
 import { createNextProject } from "../templates/nextProject.js";
 import { createNodeProject } from "../templates/nodeProject.js";
 import { createReactProject } from "../templates/reactProject.js";
 
-const execAsync = promisify(exec);
 const logMessage = logger('config:mgr');
 const makeDirAsync = promisify(fs.mkdir);
 const writeAsyncFile = promisify(fs.writeFile);
-
-async function linkGitRepository(projectDirectory, repositoryUrl) {
-    if (!repositoryUrl) {
-        logMessage.error('Repository URL not provided. Skipping linking to Git.');
-        return;
-    }
-
-    await execAsync('git init', { cwd: projectDirectory });
-    await execAsync(`git remote add origin ${repositoryUrl}`, { cwd: projectDirectory });
-
-    logMessage.highlight(`Linked project to Git repository: ${repositoryUrl}`);
-}
 
 export async function createCommand() {
     try {
