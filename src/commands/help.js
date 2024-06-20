5afc911385617e8d62130205365c7ff524c10c06
ovictorvasconcelos <victordevelopervasconcelos2006@gmail.com>
2024-06-20 21:38:42 +0100

    :sparkles: feat: create the message that appears after the '--help' command

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,3 +1,28 @@
+import logger from "../logger.js";
+
+const logMessage = logger('config:mgr');
+
 export default function helpCommand() {
+    logMessage.log(`
+    Usage: wizend [command] [options]
     
+    Commands:
+        --create                    Create a new project.
+        --delete                    Delete an existing project.
+        --start                     Start the project.
+        --link-to-github            Link the project to a GitHub repository.
+        --list-repos                List all local Git repositories.
+        --help                      Display help for all commands.
+
+    Options:
+        -h, --help                  Show this help message and exit.
+
+    Examples:
+        wizend --create             Create a new project.
+        wizend --start              Start the project.
+        wizend --delete             Delete an existing project.
+        wizend --link-to-github     Link the project to a GitHub repository.
+        wizend --list-repos         List all local Git repositories.
+
+    For more information, visit: https://github.com/ovictorvasconcelos/wizend-cli-tool`);
 }
