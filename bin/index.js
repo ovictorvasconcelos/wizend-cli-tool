b3aabd207fe4e44a78b760d8b03225c3f2b7530b
ovictorvasconcelos <victordevelopervasconcelos2006@gmail.com>
2024-06-21 09:30:32 +0100

    :sparkles: feat: add the command to update dependencies to index.js

--- bin/index.js
+++ bin/index.js
@@ -5,6 +5,7 @@ import helpCommand from "../src/commands/help.js";
 import { createCommand } from "../src/commands/create.js";
 import { deleteCommand } from "../src/commands/delete.js";
 import { linkCommand } from "../src/commands/linkRepository.js";
+import { updateCommand } from "../src/commands/updateDependencies.js";
 import { listRepositoriesCommand } from "../src/commands/listRepositories.js";
 
 const logMessage = logger('config:mgr');
@@ -15,6 +16,7 @@ try {
         '--delete': Boolean,
         '--link': Boolean,
         '--list-repos': Boolean,
+        '--update-dep': String,
         '--help': Boolean,
         '--h': Boolean,
     });
@@ -33,6 +35,11 @@ try {
     if (args['--list-repos'])
         listRepositoriesCommand();
 
+    if (args['--update-dep']) {
+        const projectDirectory = args['--update-dep'];
+        updateCommand(projectDirectory);
+    }
+
     if (args['--help'] || args['--h'])
         helpCommand();
 
@@ -49,6 +56,8 @@ function usageTool() {
     --create\t\tCreate a project
     --delete\t\tDelete a project
     --link\t\tLink to a Git repository
-    --list-repos\tList local Git repositories`);
+    --list-repos\tList local Git repositories
+    --update-dep <dir>\tUpdate project dependencies in the specified directory`);
+
     logMessage.log(' ');
 }
