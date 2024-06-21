edccbcf57e17af60d9da86eeab372d90d9deb81e
ovictorvasconcelos <victordevelopervasconcelos2006@gmail.com>
2024-06-21 09:18:29 +0100

    :sparkles: feat: check and update development dependencies

--- src/commands/updateDependencies.js
+++ src/commands/updateDependencies.js
@@ -1,8 +1,7 @@
-import path from "path";
 import { promisify } from "util";
 import logger from "../logger.js";
 import { exec } from "child_process";
-import { getLatestVersion, readPackageJson } from "../utils/packageUtils.js";
+import { getLatestVersion, readPackageJson, writePackageJson } from "../utils/packageUtils.js";
 
 const execAsync = promisify(exec);
 const logMessage = logger('config:mgr');
@@ -21,6 +20,24 @@ export async function updateCommand(projectDirectory) {
                 dependenciesUpdate.push(`${dependencies}@${latestVersion}`);
             }
         }
+
+        for (const dependencies in packageContent.devDependencies) {
+            const currentVersion = packageContent.devDependencies[dependencies];
+            const latestVersion = await getLatestVersion(dependencies);
+
+            if (latestVersion && latestVersion !== currentVersion) {
+                packageContent.devDependencies[dependencies] = latestVersion;
+                dependenciesUpdate.push(`${dependencies}@${latestVersion}`);
+            }
+        }
+
+        writePackageJson(projectDirectory, packageContent);
+
+        if (dependenciesUpdate.length > 0)
+            await execAsync(`cd ${projectDirectory} && npm install`);
+
+        logMessage.log(' ');
+        logMessage.highlight('Dependencies updated successfully!');
     } catch (error) {
         logMessage.error('Failed to update dependencies: ', error);
     }
