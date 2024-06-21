eea48bab5fa2e46c5125eb5281648d60a45b171f
ovictorvasconcelos <victordevelopervasconcelos2006@gmail.com>
2024-06-21 09:31:01 +0100

    :sparkles: chore: update the help.js help text

--- src/commands/help.js
+++ src/commands/help.js
@@ -12,17 +12,19 @@ export default function helpCommand() {
         --start                     Start the project.
         --link-to-github            Link the project to a GitHub repository.
         --list-repos                List all local Git repositories.
+        --update-dep <dir>          Update project dependencies in the specified directory.
         --help                      Display help for all commands.
 
     Options:
         -h, --help                  Show this help message and exit.
 
     Examples:
-        wizend --create             Create a new project.
-        wizend --start              Start the project.
-        wizend --delete             Delete an existing project.
-        wizend --link-to-github     Link the project to a GitHub repository.
-        wizend --list-repos         List all local Git repositories.
+        cli-tool-wizend --create             Create a new project.
+        cli-tool-wizend --start              Start the project.
+        cli-tool-wizend --delete             Delete an existing project.
+        cli-tool-wizend --link-to-github     Link the project to a GitHub repository.
+        cli-tool-wizend --list-repos         List all local Git repositories.
+        cli-tool-wizend --update-dep <dir>   Update project dependencies in the specified directory.
 
     For more information, visit: https://github.com/ovictorvasconcelos/wizend-cli-tool`);
 }
